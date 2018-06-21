c7f3547acb8946d3f9a0940d829e18dca498a5c1
Donato <donato@consorcioluiza.intranet>
2018-06-21 14:09:38 -0300

    Finanlizando plugin de paginação de tabelas

--- js.js
+++ js.js
@@ -1,10 +1,34 @@
-jQuery.fn.paginate = function(lines) {
-    console.log(this);
-    var a = this.selector + " tr:eq(" + lines + ")";
-    $(a.toString()).css("background", "#f00");
-    
-    jQuery.fn.css("background", "#f00");
-    console.log(jQuery.fn.css);
-};
 
-$("#tbTeste").paginate(3);
+(function ($) {
+    $.fn.paginate = function (amountLines, page = 1) {
+
+        if(!this.find("tfoot").length) {
+            let functionPrevious = '$("#tbTeste").paginate(' + amountLines + ', ' + (page > 1 ? page - 1 : page) + ')';
+            let functionNext = '$("#tbTeste").paginate(' + amountLines + ', ' + (page < (this.find("tbody tr").length / amountLines) ? page + 1 : page) + ')';
+
+            this.append("<tfoot>" +
+                "<tr>" +
+                    "<td colspan='" + this.find("thead th").length + "'>" +
+                        "<input class='controlsTFOOT' value='<<' type='button' onclick='" + functionPrevious + "' />" +
+                        "<label class='controlsTFOOT' id='tflbNumberPage'></label>" +
+                        "<input class='controlsTFOOT' value='>>' type='button' onclick='" + functionNext + "'/>" +
+                    "</td>" +
+                "</tr>" +
+            "</tfoot>");
+        }
+        
+        let firstLine = amountLines * (page - 1);
+        let lastLine = (amountLines * page) - 1;
+        
+        this.find("#tflbNumberPage").text(page + " de " + Math.ceil(this.find("tbody tr").length / amountLines));
+
+        this.find("tbody tr").each(function(index, elem) {
+            if(firstLine <= index && index <= lastLine) {
+                $(elem).css("display", "table-row");
+            } else {
+                $(elem).css("display", "none");
+            }
+        });
+        
+    };
+}(jQuery));
