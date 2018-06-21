c7cf9ba768ef62beeb6889dbd0f9de8ca70a468b
Donato <donato@consorcioluiza.intranet>
2018-06-21 15:38:01 -0300

    Corrigindo FIXs

--- main.js
+++ main.js
@@ -2,27 +2,31 @@
 (function ($) {
     $.fn.paginate = function (amountLines = 5, page = 1) {
 
-        if(!this.find("tfoot").length) {
-            let functionPrevious = '$("#tbTeste").paginate(' + amountLines + ', ' + (page > 1 ? page - 1 : page) + ')';
-            let functionNext = '$("#tbTeste").paginate(' + amountLines + ', ' + (page < (this.find("tbody tr").length / amountLines) ? page + 1 : page) + ')';
+        let trsTBODY = this.find("tbody tr");
 
-            this.append("<tfoot>" +
-                "<tr>" +
-                    "<td colspan='" + this.find("thead th").length + "'>" +
-                        "<input class='controlsTFOOT' value='<<' type='button' onclick='" + functionPrevious + "' />" +
-                        "<label class='controlsTFOOT' id='tflbNumberPage'></label>" +
-                        "<input class='controlsTFOOT' value='>>' type='button' onclick='" + functionNext + "'/>" +
-                    "</td>" +
-                "</tr>" +
-            "</tfoot>");
-        }
+        if(this.find("tfoot").length)
+            this.find("tfoot").remove();
+
+        let functionPrevious = '$("#' + this[0].id + '").paginate(' + amountLines + ', ' + (page > 1 ? page - 1 : page) + ');';
+        let functionNext = '$("#' + this[0].id + '").paginate(' + amountLines + ', ' + (page < (trsTBODY.length / amountLines) ? page + 1 : page) + ');';
+
+        this.append("<tfoot>" +
+            "<tr>" +
+                "<td colspan='" + this.find("thead th").length + "'>" +
+                    "<input class='controlsTFOOT' value='<<' type='button' onclick='" + functionPrevious + "' />" +
+                    "<label class='controlsTFOOT' id='tflbNumberPage'></label>" +
+                    "<input class='controlsTFOOT' value='>>' type='button' onclick='" + functionNext + "'/>" +
+                "</td>" +
+            "</tr>" +
+        "</tfoot>");
+
         
         let firstLine = amountLines * (page - 1);
         let lastLine = (amountLines * page) - 1;
         
-        this.find("#tflbNumberPage").text(page + " de " + Math.ceil(this.find("tbody tr").length / amountLines));
+        this.find("#tflbNumberPage").text(page + " de " + Math.ceil(trsTBODY.length / amountLines));
 
-        this.find("tbody tr").each(function(index, elem) {
+        trsTBODY.each(function(index, elem) {
             if(firstLine <= index && index <= lastLine) {
                 $(elem).css("display", "table-row");
             } else {
